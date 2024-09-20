670838cb630c6d49d930d3fc45b706d1f45a295f
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-09-20 11:35:16 -0400

    05-Sep20

--- ZBB/05-Sep20/Shape.h
+++ ZBB/05-Sep20/Shape.h
@@ -5,7 +5,7 @@ public:
    virtual double area( )const = 0;
    virtual std::ostream& display( std::ostream& ostr = std::cout )const = 0;
    virtual bool operator==( const Shape& S )const = 0;
-   virtual Shape* clone( )const = 0;
+   virtual Shape* clone( )const = 0 ;
    virtual ~Shape( ) = default;
 };
 std::ostream& operator<<( std::ostream& ostr, const Shape& S );
