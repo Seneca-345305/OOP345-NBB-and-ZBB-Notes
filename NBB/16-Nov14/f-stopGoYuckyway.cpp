9c1db2120a5a3b8706ca6dfb9ce527b2c583bc1b
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-11-14 12:24:31 -0500

    Nov14

--- NBB/16-Nov14/f-stopGoYuckyway.cpp
+++ NBB/16-Nov14/f-stopGoYuckyway.cpp
@@ -1,25 +1,23 @@
 #include <thread>
 #include <iostream>
-#include <thread>
-#include <iostream>
 #include <chrono>
 using namespace std;
-using clk = std::chrono::steady_clock;
+using clk = chrono::steady_clock;
 int main( ) {
    int tx, ty;
-   std::atomic<char> whoGoes = 'n';
+   atomic<char> whoGoes = 'n';
    auto Seconds = clk::now( ) + chrono::seconds( 1 );
-   std::thread tX = std::thread( 
+   thread tX(
       [&]( ) {
-         while ( whoGoes != 'x' ); 
-         for ( tx = 0; clk::now( ) < Seconds; cout << '^', tx++ ); 
-      } 
+         while ( whoGoes != 'x' );
+         for ( tx = 0; clk::now( ) < Seconds; cout << '^', tx++ );
+      }
    );
-   std::thread tY = std::thread( 
+   thread tY(
       [&]( ) {
-         while ( whoGoes != 'y' ); 
-         for ( ty = 0; clk::now( ) < Seconds; cout << '_', ty++ ); 
-      } 
+         while ( whoGoes != 'y' );
+         for ( ty = 0; clk::now( ) < Seconds; cout << '_', ty++ );
+      }
    );
    cout << "GO!" << endl;
    whoGoes = 'x';
